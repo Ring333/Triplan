448431479f3fe6c953f200c7a0befa1a64347ce5
Ring3 <ab1gale@ab1gales-MacBook-Pro.local>
2016-12-12 22:57:43 +0800

    day tab & add button
    day tab & add button

--- application/controllers/IdeaController.php
+++ application/controllers/IdeaController.php
@@ -22,18 +22,23 @@ class IdeaController extends Zend_Controller_Action
     {
         
         $request = $this->getRequest();
-        $sdate = $request->getParam("sdate");
-        $edate = $request->getParam("edate");
-        $dest = $request->getParam("dest");
-        $source = $request->getParam("source");
-        $this->view->sdate = date_create_from_format('Y-m-d', $sdate);
-        $this->view->edate = date_create_from_format('Y-m-d', $edate);
-        $this->view->source=$source;
-        $this->view->dest=$dest;
+        if ($request->getParam("new")=="true")
+        {
+            $this->view->idea=array();
+            $sdate = $request->getParam("sdate");
+            $edate = $request->getParam("edate");
+            $dest = $request->getParam("dest");
+            $source = $request->getParam("source");
+            $this->view->sdate = date_create_from_format('Y-m-d', $sdate);
+            $this->view->edate = date_create_from_format('Y-m-d', $edate);
+            $this->view->source=$source;
+            $this->view->dest=$dest;
+        }
         $replace=str_replace(" ","+",$dest);
         $getPoiInfo="https://maps.googleapis.com/maps/api/place/textsearch/xml?query=point+of+interests+in+".$replace."&key=AIzaSyD7q2AX3EtCIPd9i1ITISdayG8tQhjMXaQ";
         $poiInfo=file_get_contents($getPoiInfo);
         $this->view->xml=simplexml_load_string($poiInfo) or die ("sorry we cannot find this place");
+        
     }
 
 
